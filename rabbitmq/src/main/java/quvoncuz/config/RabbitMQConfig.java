45ef456149304c7088395e84057a12f58352e804
quvoncuz <quvoncbe.uz@gmail.com>
2026-06-03 09:32:25 +0500

    nothing just test

--- rabbitmq/src/main/java/quvoncuz/config/RabbitMQConfig.java
+++ rabbitmq/src/main/java/quvoncuz/config/RabbitMQConfig.java
@@ -1,7 +1,5 @@
 package quvoncuz.config;
 
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -18,19 +16,5 @@ public class RabbitMQConfig {
     public MessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
-
-//    @Bean
-//    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
-//            ConnectionFactory connectionFactory) {
-//        SimpleRabbitListenerContainerFactory factory =
-//                new SimpleRabbitListenerContainerFactory();
-//        factory.setConnectionFactory(connectionFactory);
-//        factory.setMessageConverter(jsonMessageConverter());
-//        return factory;
-//    }
-
-
-
-
 
 }
